24e84e8de1207092a6107610408a0907b2148db7
Aayan-Ali-Hashim <aayanali507@gmail.com>
2024-06-27 23:40:41 +0500

    first commit

---
+++ counter/app.js
@@ -0,0 +1,9 @@
+function decrease(){
+    document.querySelector('#display').innerHTML--;
+}
+function increase(){
+    document.querySelector("#display").innerHTML++;
+}
+function reset(){
+    document.querySelector("#display").innerHTML = 0;
+}
