b29a6239e18406e66dde497f6949a9b34c28a5d2
Aayan-Ali-Hashim <aayanali507@gmail.com>
2024-07-21 01:41:11 +0500

    month and year added

--- digitalclock/app.js
+++ digitalclock/app.js
@@ -1,12 +1,53 @@
 function updateTime(){
     
-    const date = new Date() 
+    const date = new Date()
+    const year = date.getFullYear()
     const hours = date.getHours()
     const minutes = date.getMinutes()
     const seconds = date.getSeconds()
     let day;
+    let month;
     const todaydate = date.getDate()
+    switch(date.getMonth()){
+        case 0:
+            month = "January";
+            break;
+        case 1:
+            month = "February";
+            break;
+        case 2:
+            month = "March";
+            break;
+        case 3:
+            month = "April";
+            break;
+        case 4:
+            month = "May";
+            break;
+        case 5:
+            month = "June";
+            break;
+        case 6:
+            month = "July";
+            break;
+        case 7:
+            month = "August";
+            break;
+        case 8:
+            month = "September";
+            break;
+        case 9:
+            month = "October";
+            break;
+        case 10:
+            month = "November";
+            break;
+        case 11:
+            month = "December";
+            break;
 
+
+    }
     switch(date.getDay()){
         case 0:
             day = "Sunday";
@@ -57,12 +98,14 @@ function updateTime(){
         document.querySelector("body").style.backgroundColor = "black";
         document.querySelector("body").style.backgroundSize = "100vh 300vh";
         document.querySelector("#headings").style.color = "white";
-        document.querySelector("#date").style.color = "white";
+        // document.querySelector("#date").style.color = "white";
         document.querySelector("#day").style.color = "white";
         document.querySelector(".greeting").innerHTML = "Good Night";
     }
 
-    document.querySelector('#date').innerHTML = todaydate;
+    document.querySelector('#date').innerHTML =  `${todaydate}/`;
+    document.querySelector('#month').innerHTML = `${month}/`;
+    document.querySelector('#year').innerHTML = year;
     document.querySelector('#day').innerHTML = day;
     document.querySelector('#hours').innerHTML =   hours < 10 ? `0${hours}` : hours;
     document.querySelector('#minutes').innerHTML = minutes  < 10 ? `0${minutes}` : minutes;
